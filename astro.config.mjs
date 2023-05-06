7359d872bd7b5769e745ccf12c1c187e8c4389f6
Alejandro Aldama <alex.aldama.rubio@gmail.com>
2023-05-06 13:35:00 -0600

    Super Molks Burger V.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://SuperMolksBurger.github.io',
+  base: '/super-molks-burger'
 });
